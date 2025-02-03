3c234d250186817f6d49bc6715b163764ea925da
caffeine-io <abidialaa@outlook.com>
2025-02-03 20:37:56 +0100

    create URL model and database connection

---
+++ src/models/url.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+    shortId: {
+      type: String,
+      required: true,
+    },
+    clicks: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+urlSchema.index({ shortId: 1 }, { unique: true });
+urlSchema.index({ originalUrl: 1 });
+export const URL = mongoose.model("URL", urlSchema);
